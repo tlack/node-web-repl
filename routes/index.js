79b2da5bc46644f0970cb1ded5529670d626d28a
root <root@wheelchair.modernmethod.com>
2012-07-23 14:40:39 -0500

    use util.inspect() to format eval response, rather than the JSON stringify ghettoness we were using

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
+var util = require('util');
 
 exports.api = function(req, res) {
 	// var eval = (1,eval);
 	var code = req.body.method+' '+req.body.params.join(' ');
-	var result = eval(code);
+	var result = util.inspect(eval(code), true, 10);
 	var out = {result:''+result, error:null, id:req.body.id};
 	res.send(JSON.stringify(out));
 };
